73adb28f143a5ad532ad60d2ce4bf1bb3e2652ff
sebastian-nunez <02snunez@gmail.com>
2023-07-04 14:25:48 -0400

    removed unused comment

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,7 +4,6 @@ import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 
 import { Icons } from "@/components/icons"
-import { ModeToggle } from "@/components/mode-toggle"
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
@@ -13,7 +12,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
     >
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logo />
+          <Icons.logo className="bg-inherit" />
           <p className="text-center text-sm leading-loose md:text-left">
             Built by{" "}
             <a
@@ -27,8 +26,6 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             .
           </p>
         </div>
-
-        {/* <ModeToggle /> */}
       </div>
     </footer>
   )
