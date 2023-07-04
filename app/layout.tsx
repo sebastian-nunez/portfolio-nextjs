3b4aa23a5caaf73b3d56ceba4c3d4ba346cc91ca
sebastian-nunez <02snunez@gmail.com>
2023-07-04 11:18:12 -0400

    footer added to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 
 import { Toaster } from "@/components/ui/toaster"
+import { SiteFooter } from "@/components/site-footer"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -80,6 +81,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
               <div className="flex-1">{children}</div>
+              <SiteFooter />
             </div>
             <TailwindIndicator />
             <Toaster />
