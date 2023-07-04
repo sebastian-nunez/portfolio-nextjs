0afb16f064e0f4aed095d54cbbe6c272b3cc6da2
sebastian-nunez <02snunez@gmail.com>
2023-07-04 11:17:56 -0400

    added a footer optimized for dark mode

---
+++ components/site-footer.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+
+import { Icons } from "@/components/icons"
+import { ModeToggle } from "@/components/mode-toggle"
+
+export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <footer
+      className={cn(className, "bg-primary text-white dark:bg-secondary")}
+    >
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+          <Icons.logo />
+          <p className="text-center text-sm leading-loose md:text-left">
+            Built by{" "}
+            <a
+              href={siteConfig.links.linkedin}
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              {siteConfig.name}
+            </a>
+            .
+          </p>
+        </div>
+
+        {/* <ModeToggle /> */}
+      </div>
+    </footer>
+  )
+}
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -15,7 +15,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background drop-shadow-sm">
+    <header className="sticky top-0 z-40 w-full border-b bg-background drop-shadow-sm dark:bg-secondary">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
 
