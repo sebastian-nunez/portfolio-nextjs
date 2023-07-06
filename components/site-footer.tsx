ed68fb9ba2b3ec1a6e110ad897a9ea8df17f072d
sebastian-nunez <02snunez@gmail.com>
2023-07-06 09:46:16 -0400

    added a tooltip to the footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -5,6 +5,13 @@ import { cn } from "@/lib/utils"
 
 import { Icons } from "@/components/icons"
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip"
+
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer
@@ -12,19 +19,26 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
     >
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logo className="bg-inherit" />
-          <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
-            <a
-              href={siteConfig.links.linkedin}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              {siteConfig.name}
-            </a>
-            .
-          </p>
+          <TooltipProvider>
+            <Tooltip>
+              <Icons.logo className="bg-inherit" />
+              <p className="text-center text-sm leading-loose md:text-left">
+                Built by{" "}
+                <TooltipTrigger>
+                  <a
+                    href={siteConfig.links.linkedin}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-medium underline underline-offset-4"
+                  >
+                    {siteConfig.name}.
+                  </a>
+                </TooltipTrigger>
+              </p>
+
+              <TooltipContent>LinkedIn</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
     </footer>
