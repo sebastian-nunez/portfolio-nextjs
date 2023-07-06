9ee5ac49eb2940daec30b3537d2ab6479d3df41a
sebastian-nunez <02snunez@gmail.com>
2023-07-06 09:45:08 -0400

    updated the home loading skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function HomeLoading() {
   return (
     <div className="container grid grid-1 gap-4 mt-9">
-      <Skeleton className="w-[200px] h-[60px] rounded md:w-[500px] lg:h-[120px]" />
-      <Skeleton className="w-[300px] h-[60px] rounded md:w-[800px] lg:h-[120px]" />
-      <Skeleton className="w-[240px] h-[30px] rounded md:w-[700px] lg:h-[45px]" />
+      <Skeleton className="w-[250px] h-[60px] sm:w-[400px] rounded md:w-[500px] lg:h-[100px]" />
+      <Skeleton className="w-[350px] h-[60px] sm:w-[500px] rounded md:w-[800px] lg:h-[100px]" />
+      <Skeleton className="w-[280px] h-[30px] sm:w-[340px] rounded md:w-[700px] lg:h-[45px]" />
       <div className="flex gap-8 mt-3">
         <Skeleton className="w-[100px] h-[40px] rounded md:w-[200px] lg:h-[60px]" />
         <Skeleton className="w-[100px] h-[40px] rounded md:w-[200px] lg:h-[60px]" />
