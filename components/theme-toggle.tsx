9e0eb1e45ba2b43aa0d2423db349f858cbedd61b
Sebastian Nunez <45319275+sebastian-nunez@users.noreply.github.com>
2023-10-26 21:59:23 +0000

    fixed a bug where the toggle theme button was being rendered inside the "TooltipTrigger" (which is also a button) within the site-header component.

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,21 +3,22 @@
 import * as React from "react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/ui/button"
+import { cn } from "@/lib/utils"
+
+import { buttonVariants } from "@/ui/button"
 import { Icons } from "@/components/icons"
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
 
   return (
-    <Button
-      variant="ghost"
-      size="sm"
+    <div
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className={cn(buttonVariants({ variant: "ghost", size: "sm" }))}
     >
       <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
-    </Button>
+    </div>
   )
 }
