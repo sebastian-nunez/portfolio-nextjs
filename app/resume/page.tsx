374006ef9e39e3d8838efe10d9b50de1b46fd403
Sebastian Nunez <45319275+sebastian-nunez@users.noreply.github.com>
2024-04-20 15:06:00 +0000

    refactor: adds `docId` for gdrive file

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,14 +3,15 @@
 import { useEffect } from "react"
 
 import { ToastAction } from "@/components/ui/toast"
-import { Anchor, H1, P } from "@/components/ui/typography"
+import { Anchor, P } from "@/components/ui/typography"
 import { useToast } from "@/components/ui/use-toast"
 
 const Resume: React.FC = () => {
   const { toast } = useToast()
+  // Document ID of the Google Drive file
+  const docId = "1hMw9q7l9Wbd2s3iO5CQcBcRqWSFI11UzgwWoaaXFabc"
 
   useEffect(() => {
-    console.log("toast")
     toast({
       title: "Awesome! Let's have a look, shall we?",
       description: "Generating a PDF preview...",
@@ -21,12 +22,14 @@ const Resume: React.FC = () => {
   return (
     <div className="h-screen" style={{ maxWidth: "100%", overflowX: "hidden" }}>
       <iframe
-        src="https://docs.google.com/gview?url=https://docs.google.com/document/d/1hMw9q7l9Wbd2s3iO5CQcBcRqWSFI11UzgwWoaaXFabc/export?format=pdf&embedded=true"
+        src={`https://docs.google.com/gview?url=https://docs.google.com/document/d/${docId}/export?format=pdf&embedded=true`}
         style={{ width: "100%", height: "100%" }}
       >
         <P>
           Unable to display PDF file.{" "}
-          <Anchor href="https://docs.google.com/document/d/1hMw9q7l9Wbd2s3iO5CQcBcRqWSFI11UzgwWoaaXFabc/export?format=pdf">
+          <Anchor
+            href={`https://docs.google.com/document/d/${docId}/export?format=pdf"`}
+          >
             Download
           </Anchor>{" "}
           instead.
